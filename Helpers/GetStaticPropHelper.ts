36cafea6e061a7f2f1ad7d6745c0e7ed525c1f7a
himadric <himadric@yahoo.com>
2023-02-15 16:21:30 -0500

    Changed to ContentFul

--- Helpers/GetStaticPropHelper.ts
+++ Helpers/GetStaticPropHelper.ts
@@ -8,132 +8,132 @@ import { container } from "tsyringe";
 import { GetStaticPropsContext } from "next";
 import { ParsedUrlQuery } from "querystring";
 import AboutPorps from "../models/AboutProps";
-import ApiHelper from "./XEdgeApiApolloClientHelper";
+import ApiHelper from "./ContentFulApiApolloClientHelper";
 
 container.register("IApiHelper", {
-	useClass: ApiHelper,
+  useClass: ApiHelper,
 });
 
 const pageComponentService = container.resolve(PageComponentService);
 const pageLayoutService = container.resolve(PageLayoutService);
 
 interface Params extends ParsedUrlQuery {
-	slug: string;
+  slug: string;
 }
 
 export default class GetStaticPropHelper {
-	static async getHomePageStaticProps(
-		introName: string,
-		collectionName: string
-	) {
-		try {
-			const mainMenuItems = await pageLayoutService.getMainMenuItems();
-			const footer = await pageLayoutService.getFooter();
-			const banner = await pageComponentService.getBanner();
-			const intro = await pageComponentService.getPageIntro(introName);
-			const topics = await pageComponentService.getTopics();
-			const blogList = await pageComponentService.getBlogs(collectionName);
-			const props: HomeProps = {
-				menu: mainMenuItems,
-				footer: footer,
-				banner: banner,
-				pageIntro: intro,
-				topics: topics,
-				blogs: blogList,
-			};
-			return {
-				props,
-				revalidate: 3600,
-			};
-		} catch (e: any) {
-			throw new Error(
-				`${introName} page static generation failed. Error: ${e.message}`
-			);
-		}
-	}
-	static async getAboutPageStaticProps() {
-		try {
-			const mainMenuItems = await pageLayoutService.getMainMenuItems();
-			const footer = await pageLayoutService.getFooter();
-			const about = await pageComponentService.getAboutContent();
-			const props: AboutPorps = {
-				menu: mainMenuItems,
-				footer: footer,
-				about: about,
-			};
-			return {
-				props,
-				revalidate: 3600,
-			};
-		} catch (e: any) {
-			throw new Error(
-				`About page static generation failed. Error: ${e.message}`
-			);
-		}
-	}
-	static async getBlogListPageStaticProps(
-		introName: string,
-		collectionName: string
-	) {
-		try {
-			const mainMenuItems = await pageLayoutService.getMainMenuItems();
-			const footer = await pageLayoutService.getFooter();
-			const intro = await pageComponentService.getPageIntro(introName);
-			const blogList = await pageComponentService.getBlogs(collectionName);
-			const props: BlogListProps = {
-				menu: mainMenuItems,
-				footer: footer,
-				pageIntro: intro,
-				blogs: blogList,
-			};
-			return {
-				props,
-				revalidate: 3600,
-			};
-		} catch (e: any) {
-			throw new Error(
-				`${introName} page static generation failed. Error: ${e.message}`
-			);
-		}
-	}
+  static async getHomePageStaticProps(
+    introName: string,
+    collectionName: string
+  ) {
+    try {
+      const mainMenuItems = await pageLayoutService.getMainMenuItems();
+      const footer = await pageLayoutService.getFooter();
+      const banner = await pageComponentService.getBanner();
+      const intro = await pageComponentService.getPageIntro(introName);
+      const topics = await pageComponentService.getTopics();
+      const blogList = await pageComponentService.getBlogs(collectionName);
+      const props: HomeProps = {
+        menu: mainMenuItems,
+        footer: footer,
+        banner: banner,
+        pageIntro: intro,
+        topics: topics,
+        blogs: blogList,
+      };
+      return {
+        props,
+        revalidate: 3600,
+      };
+    } catch (e: any) {
+      throw new Error(
+        `${introName} page static generation failed. Error: ${e.message}`
+      );
+    }
+  }
+  static async getAboutPageStaticProps() {
+    try {
+      const mainMenuItems = await pageLayoutService.getMainMenuItems();
+      const footer = await pageLayoutService.getFooter();
+      const about = await pageComponentService.getAboutContent();
+      const props: AboutPorps = {
+        menu: mainMenuItems,
+        footer: footer,
+        about: about,
+      };
+      return {
+        props,
+        revalidate: 3600,
+      };
+    } catch (e: any) {
+      throw new Error(
+        `About page static generation failed. Error: ${e.message}`
+      );
+    }
+  }
+  static async getBlogListPageStaticProps(
+    introName: string,
+    collectionName: string
+  ) {
+    try {
+      const mainMenuItems = await pageLayoutService.getMainMenuItems();
+      const footer = await pageLayoutService.getFooter();
+      const intro = await pageComponentService.getPageIntro(introName);
+      const blogList = await pageComponentService.getBlogs(collectionName);
+      const props: BlogListProps = {
+        menu: mainMenuItems,
+        footer: footer,
+        pageIntro: intro,
+        blogs: blogList,
+      };
+      return {
+        props,
+        revalidate: 3600,
+      };
+    } catch (e: any) {
+      throw new Error(
+        `${introName} page static generation failed. Error: ${e.message}`
+      );
+    }
+  }
 
-	static async getBlogListStaticPaths(collectionName: string) {
-		try {
-			const blogList = await pageComponentService.getBlogs(collectionName);
-			const paths = blogList.map((blogitem) => ({
-				params: { slug: blogitem.id.toString() },
-			}));
-			return { paths, fallback: "blocking" };
-		} catch (e: any) {
-			throw new Error(
-				`${collectionName} static path generation failed. Error: ${e.message}`
-			);
-		}
-	}
+  static async getBlogListStaticPaths(collectionName: string) {
+    try {
+      const blogList = await pageComponentService.getBlogs(collectionName);
+      const paths = blogList.map((blogitem) => ({
+        params: { slug: blogitem.id.toString() },
+      }));
+      return { paths, fallback: "blocking" };
+    } catch (e: any) {
+      throw new Error(
+        `${collectionName} static path generation failed. Error: ${e.message}`
+      );
+    }
+  }
 
-	static async getBlogPageStaticProps(
-		context: GetStaticPropsContext<ParsedUrlQuery>
-	) {
-		try {
-			const params = context.params!;
-			const mainMenuItems = await pageLayoutService.getMainMenuItems();
-			const footer = await pageLayoutService.getFooter();
-			const blog = await pageComponentService.getBlogById(
-				params.slug as string
-			);
-			const props: BlogProps = {
-				menu: mainMenuItems,
-				footer: footer,
-				blog: blog,
-			};
-			return {
-				props,
-				revalidate: 3600,
-			};
-		} catch (e: any) {
-			throw new Error(
-				`Blog page static generation failed. Error: ${e.message}`
-			);
-		}
-	}
+  static async getBlogPageStaticProps(
+    context: GetStaticPropsContext<ParsedUrlQuery>
+  ) {
+    try {
+      const params = context.params!;
+      const mainMenuItems = await pageLayoutService.getMainMenuItems();
+      const footer = await pageLayoutService.getFooter();
+      const blog = await pageComponentService.getBlogById(
+        params.slug as string
+      );
+      const props: BlogProps = {
+        menu: mainMenuItems,
+        footer: footer,
+        blog: blog,
+      };
+      return {
+        props,
+        revalidate: 3600,
+      };
+    } catch (e: any) {
+      throw new Error(
+        `Blog page static generation failed. Error: ${e.message}`
+      );
+    }
+  }
 }
